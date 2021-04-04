c1779ba18e41c30e8862aedf3ea9d40c27576d8f
amiraj nigam <amirajnigam@gmail.com>
2021-04-04 01:28:11 -0700

    queue using array

---
+++ Queue/queue_using_array.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue {
+    int size;
+    int f;
+    int r;
+    int *arr;
+};
+
+int isEmpty(struct queue *q){
+    if(q->r == q->f)
+        return 1;
+    else
+        return 0;
+}
+
+int isFULL(struct queue *q){
+    if(q->r == q->size-1)
+        return 1;
+    else
+        return 0;
+}
+
+void enqueue(struct queue *q, int val){
+    if(isFULL(q))
+        printf("This queue is Full ");
+    else{
+        q->r++;
+        q->arr[q->r] = val;
+        printf("Enqued elements:%d\n",val);
+    }
+}
+
+int dequeue(struct queue *q){
+    
+    int a = -1;
+
+    if(isEmpty(q))
+        printf("Queue is Empty");
+    else{
+        q->f++;
+        a = q->arr[q->f];  
+    }
+    return a;
+}
+
+
+int main(void)
+{
+    struct queue q;
+    q.size = 100;
+    q.f = q.r = 0;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    enqueue(&q, 10);
+    enqueue(&q, 20);
+
+
+    printf("Dequeue element:%d\n", dequeue(&q));
+    printf("Dequeue element:%d\n", dequeue(&q));
+
+
+    return 0;
+}
